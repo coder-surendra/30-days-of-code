2aab43d659c72a1861d19e992a3b5c57a9aa853d
coder-surendra <coder.suremdra@gmil.com>
2017-10-30 06:34:09 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -4,32 +4,6 @@ for i in range(n):
     k,v = input().strip().split(' ')
     dictionary[k] = v
 
-# print(dictionary)
-# accept the values for the search names
-# arr = []
-
-# flag = True
-# while(flag):
-# 	name = input()
-# 	if(name == ''):
-# 		flag = False
-# 	else:
-# 		arr.append(name)
-
-# print(arr)
-
-# length = len(arr)
-
-# i = 0
-# allKeys = dictionary.keys();
-
-# for i in range(length):
-# 	findKey = arr[i]
-# 	if findKey in allKeys:
-# 		print(findKey+'='+dictionary[findKey])
-# 	else:
-# 		print('Not found')
-
 arr = []
 j = 0
 for j in range(n):
